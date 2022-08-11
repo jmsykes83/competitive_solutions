0ecc0ab023373e68ced2598bf0ab9c0df9fd5604
Jason Sykes <jmsykes83@yahoo.com>
2022-08-11 04:37:35 -0400

    feat: came_case_four
    added the new script for the hackerrank camel case 4 competition

--- src/three_month_prep_camel_case.py
+++ src/three_month_prep_camel_case.py
@@ -4,14 +4,37 @@ of this project.
 '''
 
 import string
+import re
 
+def split(strg: string):
+    '''splits the string'''
+    return ' '.join(re.findall('[A-Za-z][^A-Z]*', strg))
 
-def get_camel_case(uncased: string) -> string:
-    '''
-    Return: cammel case code in string format
+def combine(strg:string)->string:
+    '''combines the string together'''
+    return ''.join(strg.split(' '))
 
-    '''
-    return "plastic cup"
+def str_type(typ:string,first:string, strg:string) ->string:
+    '''returns the string in a formated type'''
+    if typ == 'V':
+        return strg[:strg.find(' ')] + strg[strg.find(' '):].title() if first == 'C' else strg.lower()
+    if typ == 'M':
+        return strg[:strg.find(' ')] + strg[strg.find(' '):].title() + '()' if first != 'S' else strg.lower()
+    if typ == 'C':
+        return strg.title() if first != "S" else strg.lower()
+    return 'Failed'
+
+def set_string(strg:string)->string:
+    '''Sets the string to be evaluated'''
+    first,second,strin = strg.split(';')
+    temp_str = re.sub("[^a-zA-Z]",'',strg[4:])
+    if first == 'S':
+        temp_str = split(temp_str)
+        return str_type(second,first,temp_str)
+    if first =='C':
+        temp_str = str_type(second,first, strin)
+        return combine(temp_str)
+    return 'Failed'
 
 if __name__ == "__main__":
-    print(get_camel_case("Hello world"))
+    print(set_string(input()))
--- tests/test_camel_case.py
+++ tests/test_camel_case.py
@@ -2,40 +2,64 @@
 tEST: File for testing the camel_case project
 '''
 
-from src.three_month_prep_camel_case import get_camel_case
+from src.three_month_prep_camel_case import set_string
 
 def test_camelcase():
     '''
     camel case assertions
     '''
-    assert get_camel_case("S;M;plasticCup()") == "plastic cup"
+    assert set_string("S;M;plasticCup()") == "plastic cup"
 
 def test_mobilephone():
     '''
     camel case assertions
     '''
-    assert get_camel_case("C;V;mobile phone") == "mobilePhone"
+    assert set_string("C;V;mobile phone") == "mobilePhone"
 
 def test_coffeemachine():
     '''
     camel case assertions
     '''
-    assert get_camel_case("C;C;coffee machine") == "CoffeeMachine"
+    assert set_string("C;C;coffee machine") == "CoffeeMachine"
 
 def test_largesoftwarebook():
     '''
     camel case assertions
     '''
-    assert get_camel_case("S;C;LargeSoftwareBook") == "large software book"
+    assert set_string("S;C;LargeSoftwareBook") == "large software book"
 
 def test_whitesheetofpaper():
     '''
     camel case assertions
     '''
-    assert get_camel_case("C;M;white sheet of paper") == "whiteSheetOfPaper()"
+    assert set_string("C;M;white sheet of paper") == "whiteSheetOfPaper()"
 
 def test_pictureframe():
     '''
     camel case assertions
     '''
-    assert get_camel_case("S;V;pictureFrame") == "picture frame"
+    assert set_string("S;V;pictureFrame") == "picture frame"
+
+def test_ipad():
+    '''
+    camel case assertions
+    '''
+    assert set_string("S;V;iPad") == "i pad"
+
+def test_mousepad():
+    '''
+    camel case assertions
+    '''
+    assert set_string("C;M;mouse pad") == "mousePad()"
+
+def test_codeswarm():
+    '''
+    camel case assertions
+    '''
+    assert set_string("C;C;code swarm") == "CodeSwarm"
+
+def test_orangehighlighter():
+    '''
+    camel case assertions
+    '''
+    assert set_string("S;C;OrangeHighlighter") == "orange highlighter"
