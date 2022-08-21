20b5d1cc51821ce64ae7079d7466941a964db404
Jason Sykes <jmsykes83@yahoo.com>
2022-08-20 22:57:09 -0400

    feat: Mars Explorations
    hackerrank mars exploration solution

---
+++ src/mars_explorations.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    # Write your code here
+    # return len(s.strip())//3 - s.strip().count('SOS')
+    return sum(1 for a, b in zip(s, 'SOS'*(len(s)//3)) if a != b)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
